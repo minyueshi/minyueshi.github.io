ece84bee2ad74c1ffcf6b0c9899410a0681ccee9
minyueshi <minyueshi@gmail.com>
2015-04-30 15:32:45 -0400

    4.30

--- js/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){  
-	$('#green').hover(function(){
-        $(this).fadeTo('slow', 1);  
-	});
-  $('#pink').hover(function(){
-        $(this).fadeTo('slow', 1);  
-	});
-});
